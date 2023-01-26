afc7025a90e667882283a195560371a44488617f
Akanksha Verma <22906.akanksha@gmail.com>
2023-01-26 13:53:47 +0530

    env

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -10,7 +10,6 @@ import (
 	"os"
 
 	"github.com/gorilla/mux"
-	"github.com/joho/godotenv"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -23,18 +22,10 @@ var collection *mongo.Collection
 
 // create connection with mongo db
 func init() {
-	loadTheEnv()
 	createDBInstance()
 }
 
-func loadTheEnv() {
-	// load .env file
-	err := godotenv.Load(".env")
 
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-}
 
 func createDBInstance() {
 	// DB connection string
