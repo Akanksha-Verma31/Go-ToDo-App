b3b766307c04c9a25cee84bbf19fb059a610988f
Akanksha Verma <22906.akanksha@gmail.com>
2023-01-26 14:09:07 +0530

    port

--- main.go
+++ main.go
@@ -12,7 +12,11 @@ func main() {
 	r := router.Router()
 	// fs := http.FileServer(http.Dir("build"))
 	// http.Handle("/", fs)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 	fmt.Println("Starting server on the port 8080...")
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":" + port, r))
 }
