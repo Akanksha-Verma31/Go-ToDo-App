1ad394c4a4a3f1cd1bba302be166b00c10a517ce
Akanksha Verma <22906.akanksha@gmail.com>
2023-01-26 14:22:43 +0530

    frontend

---
+++ precache-manifest.7e05de00649f0c16f31eb3f4510d2220.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c65568a9d1d76c67eeb82ecdfc6fd14",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4fc71280813db70cf36",
+    "url": "/static/css/main.2f4546a0.chunk.css"
+  },
+  {
+    "revision": "2cf5910064bf35879bb2",
+    "url": "/static/js/2.46d3da04.chunk.js"
+  },
+  {
+    "revision": "3fa5cc1431f0d7d1c2603190736b5467",
+    "url": "/static/js/2.46d3da04.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a4fc71280813db70cf36",
+    "url": "/static/js/main.f3cb1847.chunk.js"
+  },
+  {
+    "revision": "5274233e5f14eabd9ee9",
+    "url": "/static/js/runtime-main.38a53adb.js"
+  }
+]);
